4377bc0da639b66fedb65240a8fb6107cf81dc63
HafizhAuliansyah <muhammad.hafizh.tif21@polban.ac.id>
2023-04-02 02:30:04 +0700

    Membuat Halaman Home

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+
+def main():
+    st.set_page_config(
+        page_title="Welcome"
+    )
+    st.title("Praktikum 10")
+    st.subheader("PCD D3 Hough Transform & Color")
+    st.markdown("***")
+    st.markdown("#### Nama Kelompok : Noblesse")
+    st.markdown("#### Anggota :")
+    st.markdown("- Jundiy Muhammad Alfatih (211511041)")
+    st.markdown("- Muhammad Hafizh Auliansyah (211511047)")
+    st.markdown("- Nasrulloh Fajar Muharam (211511050)")
+
+if __name__ == '__main__':
+  main()
